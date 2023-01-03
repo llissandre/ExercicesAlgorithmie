eae510e8611e824d96de2b39e17eb4a77c5cc4a2
llissandre <llissandre@hotmail.com>
2023-01-03 17:34:43 +0100

    030123 0301

--- Exercice08TableauBis/E08TableauBisProgram.cs
+++ Exercice08TableauBis/E08TableauBisProgram.cs
@@ -12,6 +12,7 @@ for (int Iterateur1 = 0; Iterateur1 <= Tableau1.Length; Iterateur1++)
         Iterateur2++;
         Iterateur1 = 0;
     }
+
     if ((Iterateur1 < Tableau1.Length) && (Iterateur2 < Tableau2.Length))
     {
         Produit = Tableau1[Iterateur1] * Tableau2[Iterateur2];
